38d6da2ebfc3d1b7c047064ea9c07979510ebae1
FTouray <ftouray9@gmail.com>
2023-10-28 23:46:46 +0100

    New files

---
+++ LoyaltyCardApp/WEB-INF/src/AddPointsServlet.java
@@ -0,0 +1,5 @@
+public class AddPointsServlet {
+    //User can add points from receipt
+    //Goes back to main page and see updated points balance
+    
+}
---
+++ LoyaltyCardApp/WEB-INF/src/SpendPointsServlet.java
@@ -0,0 +1,5 @@
+public class SpendPointsServlet {
+    //Spend points if they have enough (validation)
+    //Goed back to main page and displayes updated balance
+    
+}
