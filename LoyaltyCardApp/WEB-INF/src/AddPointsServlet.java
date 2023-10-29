5d24df7726d9e0b7c996a72b2127a282d6a69e3a
FTouray <ftouray9@gmail.com>
2023-10-29 22:54:46 +0000

    Made it into one servlet that does both actions

--- LoyaltyCardApp/WEB-INF/src/AddPointsServlet.java
+++
@@ -1,5 +0,0 @@
-public class AddPointsServlet {
-    //User can add points from receipt
-    //Goes back to main page and see updated points balance
-    
-}
--- LoyaltyCardApp/WEB-INF/src/SpendPointsServlet.java
+++
@@ -1,5 +0,0 @@
-public class SpendPointsServlet {
-    //Spend points if they have enough (validation)
-    //Goed back to main page and displayes updated balance
-    
-}
